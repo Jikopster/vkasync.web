ac0e06817994623ad1e2b812b63af46dc7b70281
Игорь Щербаков <i.v.shcherbakov@gmail.com>
2016-02-26 17:55:41 +0600

    new dependency: vksdk

--- controller/index.js
+++ controller/index.js
@@ -1,9 +1,10 @@
-var News = require(__dirname+'/../model/news.js')
+var VK = require(__dirname+'/../model/vk.js')
 
 module.exports = function(req, res) {
   var locale = res.locals.locale
-  var news = new News(locale)
-  news.get(function listener(obj) {
-    res.render('index', obj)
+  var domain = 'jikopster.vkasync'
+  var vk = new VK(locale, domain)
+  vk.get({}, function(result) {
+    res.render('index', { news: result })
   })
 }
